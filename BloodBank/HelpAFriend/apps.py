f57bdaeac88fad2e91245b6eeef3c11b00b6e0c7
Arunkumar P <128781456+Itzzarun@users.noreply.github.com>
2023-07-15 11:20:00 +0530

    Delete BloodBank/HelpAFriend directory

--- BloodBank/HelpAFriend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelpafriendConfig(AppConfig):
-    name = 'HelpAFriend'
--- BloodBank/HelpAFriend/emailSend.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-content = "Hello "
-mail = smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login('helpafriend.bloodbank@gmail.com','bloodbank.project')
-mail.sendmail('helpafriend.bloodbank@gmail.com','divyashah.qa@gmail.com',content)
